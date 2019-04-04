c4b30c2d43f9f1fe80f56151967a6219bcf32bb8
RJ <rongjihuang@gmail.com>
2019-04-05 01:15:18 +0800

    Add ModuleConfiguration for auto config the whole module

---
+++ src/main/java/tech/simter/reactive/test/ModuleConfiguration.java
@@ -0,0 +1,14 @@
+package tech.simter.reactive.test;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * All configuration for this module.
+ *
+ * @author RJ
+ */
+@Configuration("tech.simter.reactive.test.ModuleConfiguration")
+@ComponentScan("tech.simter.reactive.test")
+public class ModuleConfiguration {
+}
